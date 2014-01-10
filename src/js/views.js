9012d9f7b85ea0d393c25908257ddf83d166b290
Thibault Jouannic <thibault@miximum.fr>
2014-01-10 11:39:32 +0100

    Create basic views

--- src/js/views.js
+++ src/js/views.js
@@ -3,6 +3,38 @@ define(['backbone'], function(Backbone) {
 
     var Views = {};
 
+    Views.TimerView = Backbone.View.extend({
+        initialize: function() {
+            this.render();
+        },
+        render: function() {
+            this.$el.html('00:00');
+            return this;
+        }
+    });
+
+    Views.ControlView = Backbone.View.extend({
+        events: {
+            'click button': 'onClick'
+        },
+        initialize: function() {
+            this.render();
+        },
+        render: function() {
+            return this;
+        },
+        onClick: function(event) {
+            var target = event.target;
+            var action = target.getAttribute('data-action');
+
+            if (action == 'stop') {
+                this.trigger('timerInterrupted');
+            } else {
+                var duration = target.getAttribute('data-duration');
+                this.trigger('timerStarted', {duration: duration});
+            }
+        }
+    });
 
     return Views;
 });
