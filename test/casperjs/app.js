248435a5fc0ddaf295f5bc8318fbdd3ceecfeab1
Thibault Jouannic <thibault@miximum.fr>
2014-01-12 10:39:11 +0100

    Correct casper syntax for test suite

--- test/casperjs/app.js
+++ test/casperjs/app.js
@@ -1,6 +1,5 @@
 // Configured in Gruntfile.js
 var url = 'http://localhost:4242/index.html';
-var sinon = require('../../../node_modules/sinon/lib/sinon');
 
 casper.test.begin('The app should load flawlessly', 1, function suite(test) {
     casper.start(url, function() {
@@ -12,34 +11,26 @@ casper.test.begin('The app should load flawlessly', 1, function suite(test) {
     });
 });
 
-casper.test.begin('Start pomodoro button', {
-    setUp: function() {
-    },
+casper.test.begin('Start pomodoro button', function(test) {
+    casper.start(url, function() {
+        // Wait for requirejs to load all scripts
+        this.wait(100);
+    });
 
-    tearDown: function() {
-    },
+    casper.then(function() {
+        test.assertSelectorHasText('#timer', '00:00');
 
-    test: function(test) {
-        casper.start(url, function() {
-            // Wait for requirejs to load all scripts
-            this.wait(100);
-        });
+        this.click('#start-pomodoro-btn');
+        test.assertSelectorHasText('#timer', '25:00');
 
-        casper.then(function() {
-            test.assertSelectorHasText('#timer', '00:00');
+        this.wait(1000);
+        test.assertSelectorHasText('#timer', '24:59');
 
-            this.click('#start-pomodoro-btn');
-            test.assertSelectorHasText('#timer', '25:00');
+        this.wait(1000);
+        test.assertSelectorHasText('#timer', '24:58');
+    });
 
-            this.wait(1000);
-            test.assertSelectorHasText('#timer', '24:59');
-
-            this.wait(1000);
-            test.assertSelectorHasText('#timer', '24:58');
-        });
-
-        casper.run(function() {
-            test.done();
-        });
-    }
+    casper.run(function() {
+        test.done();
+    });
 });
