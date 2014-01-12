5d22b8ac8f650518a6c970786fdfacfe22e033a4
Thibault Jouannic <thibault@miximum.fr>
2014-01-12 12:40:57 +0100

    Testing buttons activation / deactivation

--- test/casperjs/app.js
+++ test/casperjs/app.js
@@ -1,6 +1,20 @@
 // Configured in Gruntfile.js
 var url = 'http://localhost:4242/index.html';
 
+//casper.on('remote.message', function(message) {
+//    casper.debug('>>> ' + message);
+//});
+
+
+casper.isDisabled = function(selector) {
+    "use strict";
+    this.checkStarted();
+    return this.evaluate(function _evaluate(selector) {
+        var element = __utils__.findOne(selector);
+        return element.hasAttribute('disabled');
+    }, selector);
+};
+
 casper.test.begin('The app should load flawlessly', 1, function suite(test) {
     casper.start(url, function() {
         test.assertHttpStatus(200,'http status is 200');
@@ -24,11 +38,13 @@ casper.test.begin('Start pomodoro button', function(test) {
     casper.then(function() {
         this.click('#start-pomodoro-btn');
         test.assertSelectorHasText('#timer', '25:00');
+    });
 
-        this.wait(1000);
+    casper.wait(1010, function() {
         test.assertSelectorHasText('#timer', '24:59');
+    });
 
-        this.wait(1000);
+    casper.wait(1010, function() {
         test.assertSelectorHasText('#timer', '24:58');
     });
 
@@ -44,63 +60,29 @@ casper.test.begin('Button activations', function(test) {
     });
 
     casper.then(function() {
-        test.assertEqual(
-            this.getElementAttribute('#start-pomodoro-btn', 'disabled'),
-            false
-        );
-        test.assertEqual(
-            this.getElementAttribute('#start-break-btn', 'disabled'),
-            false
-        );
-        test.assertEqual(
-            this.getElementAttribute('#start-lbreak-btn', 'disabled'),
-            false
-        );
-        test.assertEqual(
-            this.getElementAttribute('#stop-btn', 'disabled'),
-            true
-        );
+        test.assertFalsy(casper.isDisabled('#start-pomodoro-btn'));
+        test.assertFalsy(casper.isDisabled('#start-break-btn'));
+        test.assertFalsy(casper.isDisabled('#start-lbreak-btn'));
+        test.assert(casper.isDisabled('#stop-btn'));
     });
 
     casper.then(function() {
         this.click('#start-pomodoro-btn');
-
-        test.assertEqual(
-            this.getElementAttribute('#start-pomodoro-btn', 'disabled'),
-            true
-        );
-        test.assertEqual(
-            this.getElementAttribute('#start-break-btn', 'disabled'),
-            true
-        );
-        test.assertEqual(
-            this.getElementAttribute('#start-lbreak-btn', 'disabled'),
-            true
-        );
-        test.assertEqual(
-            this.getElementAttribute('#stop-btn', 'disabled'),
-            false
-        );
+        test.assert(casper.isDisabled('#start-pomodoro-btn'));
+        test.assert(casper.isDisabled('#start-break-btn'));
+        test.assert(casper.isDisabled('#start-lbreak-btn'));
+        test.assertFalsy(casper.isDisabled('#stop-btn'));
     });
 
     casper.then(function() {
         this.click('#stop-btn');
+        test.assertFalsy(casper.isDisabled('#start-pomodoro-btn'));
+        test.assertFalsy(casper.isDisabled('#start-break-btn'));
+        test.assertFalsy(casper.isDisabled('#start-lbreak-btn'));
+        test.assert(casper.isDisabled('#stop-btn'));
+    });
 
-        test.assertEqual(
-            this.getElementAttribute('#start-pomodoro-btn', 'disabled'),
-            false
-        );
-        test.assertEqual(
-            this.getElementAttribute('#start-break-btn', 'disabled'),
-            false
-        );
-        test.assertEqual(
-            this.getElementAttribute('#start-lbreak-btn', 'disabled'),
-            false
-        );
-        test.assertEqual(
-            this.getElementAttribute('#stop-btn', 'disabled'),
-            true
-        );
+    casper.run(function() {
+        test.done();
     });
 });
