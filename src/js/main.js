f08e8caa772ae6292c6e25ef6acb76a59fc134be
Thibault Jouannic <thibault@miximum.fr>
2014-01-12 16:57:42 +0100

    Add a popup to display the configuration form

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,8 @@ require.config({
         'jquery': 'js/vendor/jquery',
         'underscore': 'js/vendor/underscore',
         'backbone': 'js/vendor/backbone',
+        'foundation': 'js/vendor/foundation',
+        'reveal': 'js/vendor/foundation.reveal'
     },
     shim: {
         underscore: {
@@ -12,11 +14,15 @@ require.config({
         backbone: {
             deps: ['underscore', 'jquery'],
             exports: 'Backbone'
-        }
+        },
+        foundation: ['jquery'],
+        reveal: ['foundation']
     }
 });
 
-require(['js/app'], function(App) {
+require(['js/app', 'reveal'], function(App) {
     var app = new App();
     app.run();
+
+    $(document).foundation();
 });
